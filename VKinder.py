cf7bea519472cdbda6c616e4bc52649ebc355aa6
TatianaAndreevna <45102835+TatianaAndreevna@users.noreply.github.com>
2019-03-29 18:09:58 +0300

    Update VKinder.py

--- VKinder.py
+++ VKinder.py
@@ -29,7 +29,7 @@ def top_10(required_user):
         user_photos = search_photo_users[user[0]]
         list_photos = list()
         for photo in user_photos:
-            list_photos.append(photo[0])
+            list_photos.append(photo)
         dict_top_10['user_id'] = user[0]
         dict_top_10['user_page'] = 'https:/vk.com/id' + str(user[0])
         dict_top_10['user_photos'] = list_photos
